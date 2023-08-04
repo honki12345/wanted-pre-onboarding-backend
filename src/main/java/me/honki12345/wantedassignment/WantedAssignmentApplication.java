72078c9a009056765b1a7a55aea282b0af8ff904
honki12345 <iui4793@gmail.com>
2023-08-04 16:38:11 +0900

    Feat #1 - 스프링부트 프로젝트 초기화
    스프링부트 프로젝트 생성
    들어간 의존성 기술은:
    
    * jpa
    * web
    * lombok
    * devtools
    * h2
    * mysql driver

---
+++ src/main/java/me/honki12345/wantedassignment/WantedAssignmentApplication.java
@@ -0,0 +1,13 @@
+package me.honki12345.wantedassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/honki12345/wantedassignment/WantedAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.honki12345.wantedassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
