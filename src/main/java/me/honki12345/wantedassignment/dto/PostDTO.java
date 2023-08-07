45b8522bcb86355be690e97536ba730aa8e2aef7
honki12345 <iui4793@gmail.com>
2023-08-07 20:00:36 +0900

    Feat #6 - PostDTO 에 title 과 content 매개변수로 갖는 생성자 메서드 추가

--- src/main/java/me/honki12345/wantedassignment/dto/PostDTO.java
+++ src/main/java/me/honki12345/wantedassignment/dto/PostDTO.java
@@ -8,8 +8,11 @@ public record PostDTO(
         String content,
         Member member) {
 
+    public static PostDTO of(String title, String content) {
+        return new PostDTO(null, title, content, null);
+    }
+
     public static PostDTO of(String title, String content, Member member) {
         return new PostDTO(null, title, content, member);
     }
-
 }
