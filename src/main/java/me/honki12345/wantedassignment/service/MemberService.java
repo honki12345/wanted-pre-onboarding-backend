9971720a820ef5e56942000ec52e64f14fdaa092
honki12345 <iui4793@gmail.com>
2023-08-07 23:12:33 +0900

    Feat #5 - MemberService, PostService skeleton code 생성

---
+++ src/main/java/me/honki12345/wantedassignment/service/MemberService.java
@@ -0,0 +1,11 @@
+package me.honki12345.wantedassignment.service;
+
+import me.honki12345.wantedassignment.dto.MemberDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    public void join(MemberDTO memberDTO) {
+        // TODO
+    }
+}
---
+++ src/main/java/me/honki12345/wantedassignment/service/PostService.java
@@ -0,0 +1,29 @@
+package me.honki12345.wantedassignment.service;
+
+import me.honki12345.wantedassignment.dto.PostDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    public void create(PostDTO postDTO) {
+
+    }
+
+    public List<PostDTO> list() {
+        return null;
+    }
+
+    public PostDTO get(Long id) {
+        return null;
+    }
+
+    public void update(Long id, PostDTO postDTO) {
+
+    }
+
+    public void delete(Long id) {
+
+    }
+}
