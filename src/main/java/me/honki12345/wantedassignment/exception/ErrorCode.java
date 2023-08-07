518d4ca520e7adeec6c4ca11a4db794ed4b1f584
honki12345 <iui4793@gmail.com>
2023-08-07 23:11:13 +0900

    Feat #6 - 예외 메시지를 위한 ErrorCode 와 ErrorResponse 객체

---
+++ src/main/java/me/honki12345/wantedassignment/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package me.honki12345.wantedassignment.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    INVALID_INPUT_VALUE("올바르지 않은 입력값입니다.", "COMMON-001", HttpStatus.BAD_REQUEST),
+    NOT_FOUND("해당 값은 존재하지 않습니다", "COMMON-002", HttpStatus.NOT_FOUND);
+
+    private final String message;
+    private final String code;
+    private final HttpStatus status;
+
+    ErrorCode(String message, String code, HttpStatus status) {
+        this.message = message;
+        this.code = code;
+        this.status = status;
+    }
+}
---
+++ src/main/java/me/honki12345/wantedassignment/exception/ErrorResponse.java
@@ -0,0 +1,30 @@
+package me.honki12345.wantedassignment.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ErrorResponse {
+    private String message;
+    private String code;
+
+    private ErrorResponse(ErrorCode code) {
+        this.message = code.getMessage();
+        this.code = code.getCode();
+    }
+
+    private ErrorResponse(ErrorCode code, String message) {
+        this.message = message;
+        this.code = code.getCode();
+    }
+
+    public static ErrorResponse of(ErrorCode code) {
+        return new ErrorResponse(code);
+    }
+
+    public static ErrorResponse of(ErrorCode code, String message) {
+        return new ErrorResponse(code, message);
+    }
+}
