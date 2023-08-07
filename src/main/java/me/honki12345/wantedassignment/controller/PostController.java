89573724e8d11478f6aa436cbc0bbef6a118802d
honki12345 <iui4793@gmail.com>
2023-08-07 19:46:21 +0900

    Fest #6 - 게시글 조회 API 구현

--- src/main/java/me/honki12345/wantedassignment/controller/PostController.java
+++ src/main/java/me/honki12345/wantedassignment/controller/PostController.java
@@ -6,6 +6,7 @@ import me.honki12345.wantedassignment.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,11 @@ public class PostController {
         // TODO 페이지네이션
         List<PostDTO> postDTOs = postService.list();
         return new ResponseEntity<>(postDTOs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostDTO> get(@PathVariable Long id) {
+        PostDTO postDTO = postService.get(id);
+        return new ResponseEntity<>(postDTO, HttpStatus.OK);
     }
 }
