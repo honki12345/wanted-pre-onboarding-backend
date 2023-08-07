ea63b74a21fd4f6bd1a67bf75c3818f0f64a0a18
honki12345 <iui4793@gmail.com>
2023-08-07 23:08:34 +0900

    Test #6 - 회원가입시 유효하지 않은 값으로 인해 실패케이스 테스트 추가

--- src/test/java/me/honki12345/wantedassignment/controller/MemberControllerTest.java
+++ src/test/java/me/honki12345/wantedassignment/controller/MemberControllerTest.java
@@ -3,6 +3,7 @@ package me.honki12345.wantedassignment.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.honki12345.wantedassignment.dto.MemberDTO;
 import me.honki12345.wantedassignment.service.MemberService;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,9 +16,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(controllers = {MemberController.class})
 class MemberControllerTest {
     @Autowired
     MockMvc mockMvc;
@@ -28,13 +30,16 @@ class MemberControllerTest {
     @MockBean
     MemberService memberService;
 
+    String emailErrorMessage = "이메일 형식이 올바르지 않습니다";
+    String pwdErrorMessage = "비밀번호 형식이 올바르지 않습니다";
+
     @DisplayName("회원가입을 성공한다")
     @Test
     void join() throws Exception {
         // given
         String email = "aaaa@naver.com";
-        String pwd = "1234";
-        MemberDTO memberDTO = new MemberDTO(email, pwd);
+        String pwd = "1234888888";
+        MemberDTO memberDTO = MemberDTO.of(email, pwd);
         String requestBody = objectMapper.writeValueAsString(memberDTO);
         doNothing().when(memberService).join(memberDTO);
 
@@ -48,4 +53,70 @@ class MemberControllerTest {
                 .andExpect(status().isCreated());
     }
 
+    @DisplayName("회원가입 요청의 이메일 입력값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void joinException() throws Exception {
+        // given
+        String wrongEmail = "google.com";
+        String pwd = "1234888888";
+        MemberDTO memberDTO = MemberDTO.of(wrongEmail, pwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        doNothing().when(memberService).join(memberDTO);
+//        String errorMessage = "이메일 형식이 올바르지 않습니다";
+
+        // when // then
+        mockMvc.perform(
+                        post("/members")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(emailErrorMessage))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+
+    @DisplayName("회원가입 요청의 비밀번호 입력값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void joinException2() throws Exception {
+        // given
+        String email = "ef@google.com";
+        String wrongPwd = "188";
+        MemberDTO memberDTO = MemberDTO.of(email, wrongPwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        doNothing().when(memberService).join(memberDTO);
+
+        // when // then
+        mockMvc.perform(
+                        post("/members")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(pwdErrorMessage))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+    @DisplayName("회원가입 요청의 이메일과 비밀번호 둘 다 값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void joinException3() throws Exception {
+        // given
+        String wrongEmail = "efgoogle.com";
+        String wrongPwd = "188";
+        MemberDTO memberDTO = MemberDTO.of(wrongEmail, wrongPwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        doNothing().when(memberService).join(memberDTO);
+
+        // when // then
+        mockMvc.perform(
+                        post("/members")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(Matchers.containsString(emailErrorMessage)))
+                .andExpect(jsonPath("$.message").value(Matchers.containsString(pwdErrorMessage)))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
 }
