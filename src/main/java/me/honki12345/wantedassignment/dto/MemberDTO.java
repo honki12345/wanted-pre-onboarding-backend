4f51b9a2b213c7d572dd28ff893b498bab26b991
honki12345 <iui4793@gmail.com>
2023-08-07 23:09:08 +0900

    Feat #6 - MemberDTO email, pwd 유효성검사 어노테이션 추가

--- src/main/java/me/honki12345/wantedassignment/dto/MemberDTO.java
+++ src/main/java/me/honki12345/wantedassignment/dto/MemberDTO.java
@@ -1,8 +1,15 @@
 package me.honki12345.wantedassignment.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record MemberDTO(
         Long id,
+
+        @Pattern(regexp = ".+@.+", message = "{member.email}")
         String email,
+
+        @Size(min = 8, message = "비밀번호 형식이 올바르지 않습니다")
         String pwd) {
     public static MemberDTO of(String email, String pwd) {
         return new MemberDTO(null, email, pwd);
