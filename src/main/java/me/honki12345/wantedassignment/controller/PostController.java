4d7c06ce64b68221cad02172de6e4635522db07f
honki12345 <iui4793@gmail.com>
2023-08-07 20:01:19 +0900

    Feat #6 - 게시글 수정 API 와 게시글 삭제 API 모두 사용자 인증 기능 없이 구현

--- src/main/java/me/honki12345/wantedassignment/controller/PostController.java
+++ src/main/java/me/honki12345/wantedassignment/controller/PostController.java
@@ -5,7 +5,9 @@ import me.honki12345.wantedassignment.dto.PostDTO;
 import me.honki12345.wantedassignment.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,5 +39,22 @@ public class PostController {
     public ResponseEntity<PostDTO> get(@PathVariable Long id) {
         PostDTO postDTO = postService.get(id);
         return new ResponseEntity<>(postDTO, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Object> update(
+            @PathVariable Long id,
+            @RequestBody PostDTO postDTO
+    ) {
+        // TODO 사용자 검증
+        postService.update(id, postDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> delete(@PathVariable Long id) {
+        // TODO 사용자 검증
+        postService.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
