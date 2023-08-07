58d32703b8175ae661201c346cef90454c2ccaa2
honki12345 <iui4793@gmail.com>
2023-08-07 17:52:10 +0900

    Feat #6 - 회원가입 API 유효성 검증로직 없이 구현

---
+++ src/main/java/me/honki12345/wantedassignment/controller/MemberController.java
@@ -0,0 +1,24 @@
+package me.honki12345.wantedassignment.controller;
+
+import lombok.RequiredArgsConstructor;
+import me.honki12345.wantedassignment.dto.MemberDTO;
+import me.honki12345.wantedassignment.service.MemberService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<Object> join(@RequestBody MemberDTO memberDTO) {
+        memberService.join(memberDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
