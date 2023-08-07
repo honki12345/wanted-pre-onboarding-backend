0edb6945a210b8afce7a001d5274d5bfc7809a6f
honki12345 <iui4793@gmail.com>
2023-08-07 23:01:38 +0900

    Refactor #6 - @Valid 에서 @Validated 변경

--- src/main/java/me/honki12345/wantedassignment/controller/MemberController.java
+++ src/main/java/me/honki12345/wantedassignment/controller/MemberController.java
@@ -5,6 +5,7 @@ import me.honki12345.wantedassignment.dto.MemberDTO;
 import me.honki12345.wantedassignment.service.MemberService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity<Object> join(@RequestBody MemberDTO memberDTO) {
+    public ResponseEntity<Object> join(@Validated @RequestBody MemberDTO memberDTO) {
         // TODO 유효성 검사 및 암호화로직
         memberService.join(memberDTO);
         return new ResponseEntity<>(HttpStatus.CREATED);
