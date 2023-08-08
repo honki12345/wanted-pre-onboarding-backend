bb5c73e98ee9bae15255732aa3d495992ddec46f
honki12345 <iui4793@gmail.com>
2023-08-08 19:00:24 +0900

    Feat #7 - MemberService 회원가입 로직 구현
    이메일과 비밀번호를 저장한다. 비밀번호는 BCryptPasswordEncoder 를 이용하여 암호화하여 회원가입 정보를 저장하였다.

--- src/main/java/me/honki12345/wantedassignment/service/MemberService.java
+++ src/main/java/me/honki12345/wantedassignment/service/MemberService.java
@@ -1,11 +1,22 @@
 package me.honki12345.wantedassignment.service;
 
+import lombok.RequiredArgsConstructor;
+import me.honki12345.wantedassignment.domain.Member;
 import me.honki12345.wantedassignment.dto.MemberDTO;
+import me.honki12345.wantedassignment.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     public void join(MemberDTO memberDTO) {
-        // TODO
+        memberRepository.save(Member.builder()
+                .email(memberDTO.email())
+                .pwd(bCryptPasswordEncoder.encode(memberDTO.pwd()))
+                .build());
     }
 }
