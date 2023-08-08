1bd09a90a4815597a402db33fd47dc227f9cef4d
honki12345 <iui4793@gmail.com>
2023-08-08 16:22:40 +0900

    Feat #7 - WebSecurityConfig 추가
    h2 console 접근은 열어두고, 게시글 수정과 삭제에 대해서는 권한이 필요한걸로 설정하였다

---
+++ src/main/java/me/honki12345/wantedassignment/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package me.honki12345.wantedassignment.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebSecurityConfig {
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeHttpRequests(
+                        (auth) -> auth
+                                .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+
+                                // TODO hasRole() 통과하는지 테스트해보기
+//                                .requestMatchers(antMatcher(HttpMethod.PATCH, "posts/*")).hasRole("USER")
+                                .requestMatchers(antMatcher(HttpMethod.PATCH, "/posts/*")).authenticated()
+                                .requestMatchers(antMatcher(HttpMethod.DELETE, "/posts/*")).authenticated()
+                                .anyRequest().permitAll()
+                )
+                .build();
+    }
+}
