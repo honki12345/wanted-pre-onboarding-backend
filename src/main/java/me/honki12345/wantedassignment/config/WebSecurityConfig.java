0457f18570ce0d7e759270ddd01382c1aa364f98
honki12345 <iui4793@gmail.com>
2023-08-08 18:43:25 +0900

    Feat #7 - WebSecurityConfig 인증관리자와 패스워드인코더 설정

--- src/main/java/me/honki12345/wantedassignment/config/WebSecurityConfig.java
+++ src/main/java/me/honki12345/wantedassignment/config/WebSecurityConfig.java
@@ -4,9 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -38,5 +41,21 @@ public class WebSecurityConfig {
                                 .anyRequest().permitAll()
                 )
                 .build();
+    }
+
+    @Bean
+    public AuthenticationManager authorizationManager(HttpSecurity http,
+                                                      BCryptPasswordEncoder bCryptPasswordEncoder,
+                                                      UserDetailsService userDetailsService) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
