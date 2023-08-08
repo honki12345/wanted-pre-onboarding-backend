eb6d67a46259071eb51e847aae063153a4a06cac
honki12345 <iui4793@gmail.com>
2023-08-08 16:24:53 +0900

    Feat #5 - MemberRepository findByEmail() 추가

--- src/main/java/me/honki12345/wantedassignment/repository/MemberRepository.java
+++ src/main/java/me/honki12345/wantedassignment/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package me.honki12345.wantedassignment.repository;
 import me.honki12345.wantedassignment.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
 }
