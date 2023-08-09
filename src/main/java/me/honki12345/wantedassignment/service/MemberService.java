5607279db0dfca1945fea64643cdfa344626a516
honki12345 <iui4793@gmail.com>
2023-08-09 23:30:01 +0900

    Teat #8 - MemberService 회원가입 로직

--- src/main/java/me/honki12345/wantedassignment/service/MemberService.java
+++ src/main/java/me/honki12345/wantedassignment/service/MemberService.java
@@ -1,11 +1,23 @@
 package me.honki12345.wantedassignment.service;
 
+import lombok.RequiredArgsConstructor;
+import me.honki12345.wantedassignment.domain.Member;
 import me.honki12345.wantedassignment.dto.MemberDTO;
+import me.honki12345.wantedassignment.repository.MemberRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     public void join(MemberDTO memberDTO) {
-        // TODO
+        // TODO 중복검사
+        memberRepository.save(Member.builder()
+                .email(memberDTO.email())
+                .pwd(passwordEncoder.encode(memberDTO.pwd()))
+                .build());
     }
 }
