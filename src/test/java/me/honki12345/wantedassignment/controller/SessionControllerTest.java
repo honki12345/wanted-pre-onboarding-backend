ee585aa0f4812babad111090a7ac979178d0afd4
honki12345 <iui4793@gmail.com>
2023-08-09 23:31:12 +0900

    Docs #8 - log output 레퍼런스 주석처리

---
+++ src/test/java/me/honki12345/wantedassignment/controller/SessionControllerTest.java
@@ -0,0 +1,33 @@
+package me.honki12345.wantedassignment.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionControllerTest {
+    // TODO OutputCaptureRule 사용해서 log 테스트하기
+
+/*
+    @RunWith(SpringRunner.class)
+    @WebMvcTest(GoodGidController.class)
+    public class GoodGidControllerTest_OutputCaptureRule {
+
+        @Rule
+        public OutputCaptureRule outputCaptureRule = new OutputCaptureRule();
+
+        @Autowired
+        MockMvc mockMvc;
+
+        @Test
+        public void goodGid() throws Exception {
+
+            mockMvc.perform(get("/"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Hello goodGid"));
+
+            assertThat(outputCaptureRule.toString().contains("Slf4j"));
+            assertThat(outputCaptureRule.toString().contains("System Out"));
+        }
+    }
+*/
+
+
+}
