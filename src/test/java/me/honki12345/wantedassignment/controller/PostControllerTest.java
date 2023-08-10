0978a79a0cb06633f05a912360e960d1625eeaf9
honki12345 <iui4793@gmail.com>
2023-08-10 23:27:22 +0900

    Test - (PostControllerTest) 게시글 생성, 수정, 삭제 테스트

--- src/test/java/me/honki12345/wantedassignment/controller/PostControllerTest.java
+++ src/test/java/me/honki12345/wantedassignment/controller/PostControllerTest.java
@@ -1,6 +1,9 @@
 package me.honki12345.wantedassignment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.honki12345.wantedassignment.common.ErrorCode;
+import me.honki12345.wantedassignment.common.NotAuthorizedException;
+import me.honki12345.wantedassignment.common.PostNotFoundException;
 import me.honki12345.wantedassignment.config.SecurityConfig;
 import me.honki12345.wantedassignment.config.jwt.JwtAccessDeniedHandler;
 import me.honki12345.wantedassignment.config.jwt.JwtAuthenticationEntryPoint;
@@ -14,12 +17,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -46,16 +51,20 @@ class PostControllerTest {
     @MockBean
     PostService postService;
 
-    @DisplayName("게시글 생성을 성공한다")
+    @WithMockUser
+    @DisplayName("게시글 생성을 @WithMockUser을 통해 성공한다")
     @Test
     void create() throws Exception {
         // given
+        long id = 1L;
         String title = "title1";
         String content = "content1";
+        String author = "author";
         PostDTO postDTO = PostDTO.of(title, content);
+        PostDTO savedPostDTO = new PostDTO(id, title, content, author);
 
         String requestBody = objectMapper.writeValueAsString(postDTO);
-        doNothing().when(postService).create(postDTO);
+        when(postService.create(postDTO)).thenReturn(savedPostDTO);
 
         // when // then
         mockMvc.perform(
@@ -64,7 +73,35 @@ class PostControllerTest {
                                 .content(requestBody)
                 )
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").value(id))
+                .andExpect(jsonPath("$.title").value(title))
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.author").value(author));
+    }
+
+    @DisplayName("게시글 생성을 로그인하지 않아 권한이 없어 실패합니다")
+    @Test
+    void createException() throws Exception {
+        // given
+        long id = 1L;
+        String title = "title1";
+        String content = "content1";
+        String author = "author";
+        PostDTO postDTO = PostDTO.of(title, content);
+        PostDTO savedPostDTO = new PostDTO(id, title, content, author);
+
+        String requestBody = objectMapper.writeValueAsString(postDTO);
+        when(postService.create(postDTO)).thenReturn(savedPostDTO);
+
+        // when // then
+        mockMvc.perform(
+                        post("/posts")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(requestBody)
+                )
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
     }
 
     @DisplayName("게시글 목록조회를 성공한다")
@@ -92,7 +129,7 @@ class PostControllerTest {
                 .andExpect(jsonPath("$[1].title").value(postDTO2.title()));
     }
 
-    @DisplayName("게시글 조회를 성공한다")
+    @DisplayName("게시글 상세조회를 성공한다")
     @Test
     void getPost() throws Exception {
         // given
@@ -114,10 +151,94 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.content").value(postDTO.content()));
     }
 
-    @DisplayName("게시글 수정을 성공한다")
+    @WithMockUser
+    @DisplayName("게시글수정을 @WithMockUser을 통해 성공한다")
     @Test
     void update() throws Exception {
-        // TODO 사용자인증
+        // given
+        String title = "title1";
+        String content = "content1";
+        PostDTO postDTO = PostDTO.of(title, content);
+
+        long postId = 1L;
+        String author = "aaa@bbb.com";
+        PostDTO updatedPostDTO = new PostDTO(postId, title, content, author);
+
+        String requestBody = objectMapper.writeValueAsString(postDTO);
+        when(postService.update(postId, postDTO)).thenReturn(updatedPostDTO);
+
+        // when // then
+        mockMvc.perform(
+                        patch("/posts/{id}", postId)
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isNoContent())
+                .andExpect(jsonPath("$.id").value(postId))
+                .andExpect(jsonPath("$.title").value(title))
+                .andExpect(jsonPath("$.content").value(content))
+                .andExpect(jsonPath("$.author").value(author));
+
+    }
+
+    @DisplayName("게시글수정을 로그인 없이 요청하여 401 unauthorized 반환받는다")
+    @Test
+    void update_Login_Exception() throws Exception {
+        // given
+        String title = "title1";
+        String content = "content1";
+        PostDTO postDTO = PostDTO.of(title, content);
+
+        long postId = 1L;
+        String author = "aaa@bbb.com";
+        PostDTO updatedPostDTO = new PostDTO(postId, title, content, author);
+
+        String requestBody = objectMapper.writeValueAsString(postDTO);
+        when(postService.update(postId, postDTO)).thenReturn(updatedPostDTO);
+
+        // when // then
+        mockMvc.perform(
+                        patch("/posts/{id}", postId)
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser
+    @DisplayName("게시글수정을 해당 id 의 게시글이 존재하지 않아 404 NotFound 반환한다")
+    @Test
+    void update_Post_Exception() throws Exception {
+        // given
+        long postId = 1L;
+
+        String title = "title1";
+        String content = "content1";
+        PostDTO postDTO = PostDTO.of(title, content);
+        String requestBody = objectMapper.writeValueAsString(postDTO);
+
+        PostNotFoundException postNotFoundException = new PostNotFoundException();
+        doThrow(postNotFoundException).when(postService).update(postId, postDTO);
+
+        // when // then
+        mockMvc.perform(
+                        patch("/posts/{id}", postId)
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(postNotFoundException.getMessage()))
+                .andExpect(jsonPath("$.code").value(ErrorCode.NOT_FOUND.getCode()));
+
+    }
+
+    @WithMockUser
+    @DisplayName("게시글수정을 게시글 작성자가 아닌 유저가 요청하여 403 forbidden 반환받는다")
+    @Test
+    void update_Author_Exception() throws Exception {
         // given
         long postId = 1L;
 
@@ -127,7 +248,7 @@ class PostControllerTest {
 
         String requestBody = objectMapper.writeValueAsString(postDTO);
 
-        doNothing().when(postService).update(postId, postDTO);
+        when(postService.update(postId, postDTO)).thenThrow(NotAuthorizedException.class);
 
         // when // then
         mockMvc.perform(
@@ -136,13 +257,16 @@ class PostControllerTest {
                                 .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
-                .andExpect(status().isNoContent());
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(ErrorCode.NOT_AUTHORIZED.getMessage()))
+                .andExpect(jsonPath("$.code").value(ErrorCode.NOT_AUTHORIZED.getCode()));
+
     }
 
-    @DisplayName("게시글 삭제에 성공한다")
+    @WithMockUser
+    @DisplayName("게시글삭제를 @WithMockUser을 통해 성공한다")
     @Test
     void deletePost() throws Exception {
-        // TODO 사용자인증
         // given
         long postId = 1L;
 
@@ -154,5 +278,60 @@ class PostControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isNoContent());
+    }
+
+    @DisplayName("게시글삭제를 로그인하지 않아 401 UnAuthorized 받는다")
+    @Test
+    void deletePost_Login_Exception () throws Exception {
+        // given
+        long postId = 1L;
+
+        doNothing().when(postService).delete(postId);
+
+        // when // then
+        mockMvc.perform(
+                        delete("/posts/{id}", postId)
+                )
+                .andDo(print())
+                .andExpect(status().isUnauthorized());
+    }
+
+    @WithMockUser
+    @DisplayName("게시글삭제를 해당 id 의 게시글이 없어서 404 NotFound 받는다")
+    @Test
+    void deletePost_Post_Exception() throws Exception {
+        // given
+        long postId = 1L;
+        PostNotFoundException postNotFoundException = new PostNotFoundException();
+
+        doThrow(postNotFoundException).when(postService).delete(postId);
+
+        // when // then
+        mockMvc.perform(
+                        delete("/posts/{id}", postId)
+                )
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value(postNotFoundException.getMessage()))
+                .andExpect(jsonPath("$.code").value(ErrorCode.NOT_FOUND.getCode()));
+    }
+
+    @WithMockUser
+    @DisplayName("게시글삭제를 해당 사용자가 아니어서 403 Forbidden 받는다")
+    @Test
+    void deletePost_Author_Exception() throws Exception {
+        // given
+        long postId = 1L;
+
+        doThrow(new NotAuthorizedException()).when(postService).delete(postId);
+
+        // when // then
+        mockMvc.perform(
+                        delete("/posts/{id}", postId)
+                )
+                .andDo(print())
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.message").value(ErrorCode.NOT_AUTHORIZED.getMessage()))
+                .andExpect(jsonPath("$.code").value(ErrorCode.NOT_AUTHORIZED.getCode()));
     }
 }
