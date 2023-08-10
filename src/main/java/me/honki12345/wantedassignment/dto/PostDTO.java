5aecabb0cef2ab46bddddc440479724bd50a86c9
honki12345 <iui4793@gmail.com>
2023-08-10 23:27:45 +0900

    Feat - (PostDTO) PostDTO from(Post)

--- src/main/java/me/honki12345/wantedassignment/dto/PostDTO.java
+++ src/main/java/me/honki12345/wantedassignment/dto/PostDTO.java
@@ -1,18 +1,24 @@
 package me.honki12345.wantedassignment.dto;
 
 import me.honki12345.wantedassignment.domain.Member;
+import me.honki12345.wantedassignment.domain.Post;
 
 public record PostDTO(
         Long id,
         String title,
         String content,
-        Member member) {
+        String author) {
 
     public static PostDTO of(String title, String content) {
         return new PostDTO(null, title, content, null);
     }
 
-    public static PostDTO of(String title, String content, Member member) {
-        return new PostDTO(null, title, content, member);
+    public static PostDTO from(Post post) {
+        return new PostDTO(
+                post.getId(),
+                post.getTitle(),
+                post.getContent(),
+                post.getMember().getEmail()
+        );
     }
 }
