8556c22d302f9d3a70284863a6ac52a81b16291f
honki12345 <iui4793@gmail.com>
2023-08-10 16:12:23 +0900

    Test #8 - (JWTUtil) validateToken() 테스트

--- src/test/java/me/honki12345/wantedassignment/config/security/JWTUtilTest.java
+++ src/test/java/me/honki12345/wantedassignment/config/security/JWTUtilTest.java
@@ -1,6 +1,7 @@
 package me.honki12345.wantedassignment.config.security;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -30,7 +31,7 @@ class JWTUtilTest {
 
     @DisplayName("JWT 을 생성하면 특정 로그가 출력된다")
     @Test
-    void test(CapturedOutput output) {
+    void generateToken(CapturedOutput output) {
         // Given
         Map<String, Object> claimMap = Map.of("email", "pwd");
         String message = "generateKey..." + key;
@@ -40,8 +41,25 @@ class JWTUtilTest {
 
         // Then
         assertTrue(output.getOut().contains(message));
+        log.info(jwtString);
 
+    }
 
+    @DisplayName("JWT 을 검증한다")
+    @Test
+    void validateToken(CapturedOutput output) {
+        // Given
+        String jwtString = jwtUtil.generateToken(
+                Map.of("mid", "aaaa",
+                        "email", "aaaa@bbbb.com"),
+                1);
+
+        // When
+        Map<String, Object> claim = jwtUtil.validateToken(jwtString);
+
+        // Then
+        Assertions.assertThat(claim.get("mid")).isEqualTo("aaaa");
+        Assertions.assertThat(claim.get("email")).isEqualTo("aaaa@bbbb.com");
     }
 
 }
