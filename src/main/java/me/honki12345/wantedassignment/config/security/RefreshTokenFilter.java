69b4759c5222a45547752aca58a122bd355a3091
honki12345 <iui4793@gmail.com>
2023-08-10 16:16:19 +0900

    Feat #8 - RefreshTokenFilter

---
+++ src/main/java/me/honki12345/wantedassignment/config/security/RefreshTokenFilter.java
@@ -0,0 +1,33 @@
+package me.honki12345.wantedassignment.config.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RefreshTokenFilter extends OncePerRequestFilter {
+    private final String refreshPath;
+    private final JWTUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getRequestURI();
+
+        if (!path.equals(refreshPath)) {
+            log.info("skip refresh token filter...");
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        log.info("Refresh Token Filter...run....");
+    }
+}
