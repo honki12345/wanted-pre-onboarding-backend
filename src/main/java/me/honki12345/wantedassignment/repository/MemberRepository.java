970f5045d4d03845f214c83271bd8545b2ec572a
honki12345 <iui4793@gmail.com>
2023-08-10 18:39:43 +0900

    Feat #9 - (MemberRepository) 연관된 권한객체와 조회메소드

--- src/main/java/me/honki12345/wantedassignment/repository/MemberRepository.java
+++ src/main/java/me/honki12345/wantedassignment/repository/MemberRepository.java
@@ -1,10 +1,14 @@
 package me.honki12345.wantedassignment.repository;
 
 import me.honki12345.wantedassignment.domain.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Member> findOneWithAuthoritiesByEmail(String email);
 }
