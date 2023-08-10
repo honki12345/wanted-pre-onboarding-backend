baf3f6e04b501ca5e67588b769d29dba519f008d
honki12345 <iui4793@gmail.com>
2023-08-10 20:42:03 +0900

    Feat #9 - (Authority.java) Builder 추가

--- src/main/java/me/honki12345/wantedassignment/domain/Authority.java
+++ src/main/java/me/honki12345/wantedassignment/domain/Authority.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Authority {
     @Id
     @Column(name = "authority_name")
