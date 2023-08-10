e2f6e357d5318bba772a0f25bf908817a7222ff8
honki12345 <iui4793@gmail.com>
2023-08-10 18:38:54 +0900

    Feat #9 - (CustomUserDetailsService) loadUserByUsername()

---
+++ src/main/java/me/honki12345/wantedassignment/service/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package me.honki12345.wantedassignment.service;
+
+import lombok.RequiredArgsConstructor;
+import me.honki12345.wantedassignment.domain.Member;
+import me.honki12345.wantedassignment.repository.MemberRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return memberRepository.findOneWithAuthoritiesByEmail(email)
+                .map(member -> createUser(email, member))
+                .orElseThrow(() -> new UsernameNotFoundException(email + " -> 존재하지 않습니다"));
+    }
+
+    private User createUser(String email, Member member) {
+        List<SimpleGrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+
+        return new User(
+                member.getEmail(),
+                member.getPwd(),
+                grantedAuthorities
+        );
+    }
+}
