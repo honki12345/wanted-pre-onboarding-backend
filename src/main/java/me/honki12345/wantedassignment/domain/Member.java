d56a05c8bac471fbe9763995b090a68e71787ff3
honki12345 <iui4793@gmail.com>
2023-08-10 17:48:47 +0900

    Feat #9 - (Member.java) @ManyToMany
    Member 엔터티와 Authority 엔터티의 다대다 관계 테이블을 위한 설정을 하였다.

--- src/main/java/me/honki12345/wantedassignment/domain/Member.java
+++ src/main/java/me/honki12345/wantedassignment/domain/Member.java
@@ -5,12 +5,16 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.Objects;
+import java.util.Set;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,6 +29,13 @@ public class Member {
 
     @Column(nullable = false)
     private String pwd;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
 
     @Builder
     private Member(String email, String pwd) {
