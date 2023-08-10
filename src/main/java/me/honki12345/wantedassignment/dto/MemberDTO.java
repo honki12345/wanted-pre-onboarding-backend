c8befb3420eeb47dcb5f32cc55fb1238757bc8fb
honki12345 <iui4793@gmail.com>
2023-08-10 18:37:43 +0900

    Feat #9 - (MemberDTO) Set<AuthorityDTO> 필드추가
    DB 와 서비스 계층간에도 이 데이터 전송객체를 사용하기 위해 속성을 추가하였다.
    컨트롤러와 서비스 간에도 이 객체를 사용해서 좋지 않은 선택이지만 우선은 개발 생산성을 위해 선택하였다.

--- src/main/java/me/honki12345/wantedassignment/dto/MemberDTO.java
+++ src/main/java/me/honki12345/wantedassignment/dto/MemberDTO.java
@@ -2,6 +2,10 @@ package me.honki12345.wantedassignment.dto;
 
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import me.honki12345.wantedassignment.domain.Member;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public record MemberDTO(
         Long id,
@@ -10,8 +14,28 @@ public record MemberDTO(
         String email,
 
         @Size(min = 8, message = "비밀번호 형식이 올바르지 않습니다")
-        String pwd) {
+        String pwd,
+
+        Set<AuthorityDTO> authorityDTOSet
+
+) {
     public static MemberDTO of(String email, String pwd) {
-        return new MemberDTO(null, email, pwd);
+        return new MemberDTO(null, email, pwd, null);
     }
+
+    public static MemberDTO from(Member member) {
+        if (member == null) {
+            return null;
+        }
+
+        return new MemberDTO(
+                null,
+                member.getEmail(),
+                member.getPwd(),
+                member.getAuthorities().stream()
+                        .map(authority -> new AuthorityDTO(authority.getAuthorityName()))
+                        .collect(Collectors.toSet())
+        );
+    }
+
 }
