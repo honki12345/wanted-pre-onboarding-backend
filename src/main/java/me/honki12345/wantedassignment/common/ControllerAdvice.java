f6818f458fbeb38fdad967835ce06a2329414287
honki12345 <iui4793@gmail.com>
2023-08-10 23:25:19 +0900

    Refactor - (ControllerAdvice) 패키지이동

--- src/main/java/me/honki12345/wantedassignment/common/ControllerAdvice.java
+++ src/main/java/me/honki12345/wantedassignment/common/ControllerAdvice.java
@@ -1,7 +1,5 @@
-package me.honki12345.wantedassignment.controller;
+package me.honki12345.wantedassignment.common;
 
-import me.honki12345.wantedassignment.common.ErrorCode;
-import me.honki12345.wantedassignment.common.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,5 +17,21 @@ public class ControllerAdvice {
         return new ResponseEntity<>(
                 ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, sj.toString()),
                 ErrorCode.INVALID_INPUT_VALUE.getStatus());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.NOT_FOUND, e.getMessage()),
+                ErrorCode.NOT_FOUND.getStatus()
+        );
+    }
+
+    @ExceptionHandler(NotAuthorizedException.class)
+    public ResponseEntity<ErrorResponse> notAuthorizedException(NotAuthorizedException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.NOT_AUTHORIZED),
+                ErrorCode.NOT_AUTHORIZED.getStatus()
+        );
     }
 }
