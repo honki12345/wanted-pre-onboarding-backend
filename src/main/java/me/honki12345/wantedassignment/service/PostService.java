c3da822eb03a79f1659e768d3b26737990a0c505
honki12345 <iui4793@gmail.com>
2023-08-10 20:47:06 +0900

    Feat #9 - (PostService) 작성자 검증로직

--- src/main/java/me/honki12345/wantedassignment/service/PostService.java
+++ src/main/java/me/honki12345/wantedassignment/service/PostService.java
@@ -1,10 +1,14 @@
 package me.honki12345.wantedassignment.service;
 
+import lombok.extern.slf4j.Slf4j;
+import me.honki12345.wantedassignment.domain.Post;
 import me.honki12345.wantedassignment.dto.PostDTO;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PostService {
     public void create(PostDTO postDTO) {
@@ -20,10 +24,17 @@ public class PostService {
     }
 
     public void update(Long id, PostDTO postDTO) {
-
     }
 
     public void delete(Long id) {
 
+    }
+
+    private void authorizePostAuthor(Post post) {
+        String userName
+                = SecurityContextHolder.getContext().getAuthentication().getName();
+        if (!post.getAuthor().equals(userName)) {
+            throw new IllegalArgumentException("not authorized");
+        }
     }
 }
