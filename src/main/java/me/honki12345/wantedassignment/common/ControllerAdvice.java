4f4be2a4fb963595a1f51e2198d873264ae7d9bf
honki12345 <iui4793@gmail.com>
2023-08-11 09:53:10 +0900

    Refactor - ControllerAdvice
    각 @ExceptionHandler 마다 가독성을 위해 주석으로 ErrorCode 값을 넣었다

--- src/main/java/me/honki12345/wantedassignment/common/ControllerAdvice.java
+++ src/main/java/me/honki12345/wantedassignment/common/ControllerAdvice.java
@@ -9,6 +9,44 @@ import java.util.StringJoiner;
 
 @RestControllerAdvice
 public class ControllerAdvice {
+    //    EMAIL_DUPLICATE("중복된 이메일이 존재합니다", "MEMBER-002", HttpStatus.BAD_REQUEST),
+    @ExceptionHandler(DuplicateEmailException.class)
+    public ResponseEntity<ErrorResponse> duplicateEmailException(DuplicateEmailException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.EMAIL_DUPLICATE),
+                ErrorCode.EMAIL_DUPLICATE.getStatus()
+        );
+    }
+
+
+//    POST_NOT_FOUND("해당 게시글을 찾을 수 없습니다", "POST-001", HttpStatus.NOT_FOUND);
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<ErrorResponse> postNotFoundException(PostNotFoundException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.POST_NOT_FOUND),
+                ErrorCode.POST_NOT_FOUND.getStatus()
+        );
+    }
+
+//    MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다", "MEMBER-001", HttpStatus.NOT_FOUND),
+    @ExceptionHandler(MemberNotFoundException.class)
+    public ResponseEntity<ErrorResponse> memberNotFoundException(MemberNotFoundException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.MEMBER_NOT_FOUND),
+                ErrorCode.MEMBER_NOT_FOUND.getStatus()
+        );
+    }
+
+//    NOT_FOUND("해당 값은 존재하지 않습니다", "COMMON-002", HttpStatus.NOT_FOUND),
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException e) {
+        return new ResponseEntity<>(
+                ErrorResponse.of(ErrorCode.NOT_FOUND),
+                ErrorCode.NOT_FOUND.getStatus()
+        );
+    }
+
+//    INVALID_INPUT_VALUE("올바르지 않은 입력값입니다.", "COMMON-001",HttpStatus.BAD_REQUEST),
     @ExceptionHandler(BindException.class)
     public ResponseEntity<ErrorResponse> bindException(BindException e) {
         StringJoiner sj = new StringJoiner(", ");
@@ -19,14 +57,7 @@ public class ControllerAdvice {
                 ErrorCode.INVALID_INPUT_VALUE.getStatus());
     }
 
-    @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorResponse> notFoundException(NotFoundException e) {
-        return new ResponseEntity<>(
-                ErrorResponse.of(ErrorCode.NOT_FOUND, e.getMessage()),
-                ErrorCode.NOT_FOUND.getStatus()
-        );
-    }
-
+//    NOT_AUTHORIZED("권한이 없습니다", "COMMON-003", HttpStatus.FORBIDDEN),
     @ExceptionHandler(NotAuthorizedException.class)
     public ResponseEntity<ErrorResponse> notAuthorizedException(NotAuthorizedException e) {
         return new ResponseEntity<>(
