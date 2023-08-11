890121159b6ae151e9b6d403b8056197d6a8dec4
honki12345 <iui4793@gmail.com>
2023-08-11 09:55:44 +0900

    Feat - (ErrorCode) 값 추가
    MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다", "MEMBER-001", HttpStatus.NOT_FOUND),
    POST_NOT_FOUND("해당 게시글을 찾을 수 없습니다", "POST-001", HttpStatus.NOT_FOUND),
    EMAIL_DUPLICATE("중복된 이메일이 존재합니다", "MEMBER-002", HttpStatus.BAD_REQUEST)

--- src/main/java/me/honki12345/wantedassignment/common/ErrorCode.java
+++ src/main/java/me/honki12345/wantedassignment/common/ErrorCode.java
@@ -7,7 +7,12 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INVALID_INPUT_VALUE("올바르지 않은 입력값입니다.", "COMMON-001", HttpStatus.BAD_REQUEST),
     NOT_FOUND("해당 값은 존재하지 않습니다", "COMMON-002", HttpStatus.NOT_FOUND),
-    NOT_AUTHORIZED("권한이 없습니다", "COMMON-003", HttpStatus.FORBIDDEN);
+    NOT_AUTHORIZED("권한이 없습니다", "COMMON-003", HttpStatus.FORBIDDEN),
+
+    MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다", "MEMBER-001", HttpStatus.NOT_FOUND),
+    EMAIL_DUPLICATE("중복된 이메일이 존재합니다", "MEMBER-002", HttpStatus.BAD_REQUEST),
+
+    POST_NOT_FOUND("해당 게시글을 찾을 수 없습니다", "POST-001", HttpStatus.NOT_FOUND);
 
     private final String message;
     private final String code;
