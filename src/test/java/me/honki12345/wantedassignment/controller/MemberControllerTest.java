2b8d8a8acd5b1611370cf23fa5fdaeccbf029eb5
honki12345 <iui4793@gmail.com>
2023-08-11 09:56:31 +0900

    Feat - (MemberControllerTest) 회원가입 중복예외 테스트추가

--- src/test/java/me/honki12345/wantedassignment/controller/MemberControllerTest.java
+++ src/test/java/me/honki12345/wantedassignment/controller/MemberControllerTest.java
@@ -1,6 +1,8 @@
 package me.honki12345.wantedassignment.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.honki12345.wantedassignment.common.DuplicateEmailException;
+import me.honki12345.wantedassignment.common.ErrorCode;
 import me.honki12345.wantedassignment.config.SecurityConfig;
 import me.honki12345.wantedassignment.config.jwt.JwtAccessDeniedHandler;
 import me.honki12345.wantedassignment.config.jwt.JwtAuthenticationEntryPoint;
@@ -17,7 +19,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -46,13 +47,12 @@ class MemberControllerTest {
 
     @DisplayName("회원가입을 성공한다")
     @Test
-    void join() throws Exception {
+    void signup() throws Exception {
         // given
         String email = "aaaa@naver.com";
         String pwd = "1234888888";
         MemberDTO memberDTO = MemberDTO.of(email, pwd);
         String requestBody = objectMapper.writeValueAsString(memberDTO);
-//        doNothing().when(memberService).signup(memberDTO);
         when(memberService.signup(memberDTO)).thenReturn(memberDTO);
 
         // when // then
@@ -67,7 +67,7 @@ class MemberControllerTest {
 
     @DisplayName("회원가입 요청의 이메일 입력값이 올바르지 않아 예외가 발생한다")
     @Test
-    void joinException() throws Exception {
+    void signupException() throws Exception {
         // given
         String wrongEmail = "google.com";
         String pwd = "1234888888";
@@ -91,7 +91,7 @@ class MemberControllerTest {
 
     @DisplayName("회원가입 요청의 비밀번호 입력값이 올바르지 않아 예외가 발생한다")
     @Test
-    void joinException2() throws Exception {
+    void signupException2() throws Exception {
         // given
         String email = "ef@google.com";
         String wrongPwd = "188";
@@ -113,7 +113,7 @@ class MemberControllerTest {
     }
     @DisplayName("회원가입 요청의 이메일과 비밀번호 둘 다 값이 올바르지 않아 예외가 발생한다")
     @Test
-    void joinException3() throws Exception {
+    void signupException3() throws Exception {
         // given
         String wrongEmail = "efgoogle.com";
         String wrongPwd = "188";
@@ -133,5 +133,27 @@ class MemberControllerTest {
                 .andExpect(jsonPath("$.message").value(Matchers.containsString(emailErrorMessage)))
                 .andExpect(jsonPath("$.message").value(Matchers.containsString(pwdErrorMessage)))
                 .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+
+    @DisplayName("회원가입시 중복된 이메일이 있어 400 BadRequest 반환한다")
+    @Test
+    void signup_Duplicate_Email_Exception() throws Exception {
+        // Given
+        String email = "aaaa@naver.com";
+        String pwd = "1234888888";
+        MemberDTO memberDTO = MemberDTO.of(email, pwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        when(memberService.signup(memberDTO)).thenThrow(DuplicateEmailException.class);
+
+        // When // Then
+        mockMvc.perform(
+                        post("/members")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(ErrorCode.EMAIL_DUPLICATE.getMessage()))
+                .andExpect(jsonPath("$.code").value(ErrorCode.EMAIL_DUPLICATE.getCode()));
     }
 }
