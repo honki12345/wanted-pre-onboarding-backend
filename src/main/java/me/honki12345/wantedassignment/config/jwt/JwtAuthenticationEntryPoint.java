fcc54b55ce105d147b533c61a52bc206119dbb58
honki12345 <iui4793@gmail.com>
2023-08-11 19:13:46 +0900

    Feat - (JwtAuthEntryPoint) ExceptionHandling 구현

--- src/main/java/me/honki12345/wantedassignment/config/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/me/honki12345/wantedassignment/config/jwt/JwtAuthenticationEntryPoint.java
@@ -1,18 +1,32 @@
 package me.honki12345.wantedassignment.config.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.honki12345.wantedassignment.common.ErrorCode;
+import me.honki12345.wantedassignment.common.ErrorResponse;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setStatus(ErrorCode.NEED_LOGIN.getStatus().value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(objectMapper.writeValueAsString(ErrorResponse.of(ErrorCode.NEED_LOGIN)));
+
     }
 }
