c1dade3ba0d5ab18189d79eaa72cc3d310f4759c
honki12345 <iui4793@gmail.com>
2023-08-12 01:15:29 +0900

    Refactor - (PostService) 변경된 DTO 적용

--- src/main/java/me/honki12345/wantedassignment/service/PostService.java
+++ src/main/java/me/honki12345/wantedassignment/service/PostService.java
@@ -4,11 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.honki12345.wantedassignment.common.MemberNotFoundException;
 import me.honki12345.wantedassignment.common.NotAuthorizedException;
-import me.honki12345.wantedassignment.common.NotFoundException;
 import me.honki12345.wantedassignment.common.PostNotFoundException;
+import me.honki12345.wantedassignment.controller.dto.PostCreateRequestDTO;
+import me.honki12345.wantedassignment.controller.dto.PostCreateResponseDTO;
+import me.honki12345.wantedassignment.controller.dto.PostGetResponseDTO;
+import me.honki12345.wantedassignment.controller.dto.PostUpdateRequestDTO;
+import me.honki12345.wantedassignment.controller.dto.PostUpdateResponseDTO;
 import me.honki12345.wantedassignment.domain.Member;
 import me.honki12345.wantedassignment.domain.Post;
-import me.honki12345.wantedassignment.dto.PostDTO;
 import me.honki12345.wantedassignment.repository.MemberRepository;
 import me.honki12345.wantedassignment.repository.PostRepository;
 import org.springframework.data.domain.Page;
@@ -27,46 +30,41 @@ public class PostService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public PostDTO create(PostDTO postDTO) {
+    public PostCreateResponseDTO create(PostCreateRequestDTO requestDTO) {
         Member member = memberRepository.findByEmail(getUsername())
                .orElseThrow(MemberNotFoundException::new);
 
         Post post = Post.builder()
-                .title(postDTO.title())
-                .content(postDTO.content())
+                .title(requestDTO.title())
+                .content(requestDTO.content())
                 .member(member)
                 .build();
 
-        return PostDTO.from(postRepository.save(post));
+        return PostCreateResponseDTO.from(postRepository.save(post));
     }
 
     @Transactional(readOnly = true)
-    public List<PostDTO> list(Pageable pageable) {
+    public List<PostGetResponseDTO> list(Pageable pageable) {
         Page<Post> posts = postRepository.findAll(pageable);
-        return posts.map(post ->
-                        PostDTO.of(
-                                String.valueOf(post.getId()),
-                                post.getTitle(),
-                                post.getContent(),
-                                post.getMember().getEmail()))
-                .stream().toList();
+        return posts.map(PostGetResponseDTO::from)
+                        .stream().toList();
     }
 
     @Transactional(readOnly = true)
-    public PostDTO get(Long id) {
+    public PostGetResponseDTO get(Long id) {
         Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
-        return PostDTO.from(post);
+        return PostGetResponseDTO.from(post);
     }
 
     @Transactional
-    public PostDTO update(Long id, PostDTO postDTO) {
+    public PostUpdateResponseDTO update(Long id, PostUpdateRequestDTO postDTO) {
         Post post = postRepository.findById(id).orElseThrow(PostNotFoundException::new);
         authorizePostAuthor(post);
 
         post.update(postDTO);
         Post savedPost = postRepository.save(post);
 
-        return PostDTO.from(savedPost);
+        return PostUpdateResponseDTO.from(savedPost);
     }
 
     public void delete(Long id) {
