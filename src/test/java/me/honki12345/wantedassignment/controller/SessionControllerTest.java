0923cf9d36fdc265b43b90a3e68d2c7f9b380761
honki12345 <iui4793@gmail.com>
2023-08-11 13:38:11 +0900

    Feat #6 - (SessionControllerTest) 로그인 테스트
    로그인 성공 케이스와 이메일, 비밀번호, 이메일과 비밀번호 둘 다 validation 실패 케이스를 테스트하였다.

---
+++ src/test/java/me/honki12345/wantedassignment/controller/SessionControllerTest.java
@@ -0,0 +1,138 @@
+package me.honki12345.wantedassignment.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.honki12345.wantedassignment.config.SecurityConfig;
+import me.honki12345.wantedassignment.config.jwt.JwtAccessDeniedHandler;
+import me.honki12345.wantedassignment.config.jwt.JwtAuthenticationEntryPoint;
+import me.honki12345.wantedassignment.config.jwt.TokenProvider;
+import me.honki12345.wantedassignment.dto.MemberDTO;
+import me.honki12345.wantedassignment.service.SessionService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = {SessionController.class})
+@Import({
+        SecurityConfig.class,
+        TokenProvider.class,
+        JwtAuthenticationEntryPoint.class,
+        JwtAccessDeniedHandler.class
+})
+class SessionControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @MockBean
+    SessionService sessionService;
+
+    final String EMAIL_VALID_MSG = "이메일 형식이 올바르지 않습니다";
+    final String PWD_VALID_MSG = "비밀번호 형식이 올바르지 않습니다";
+
+    @DisplayName("로그인에 성공한다")
+    @Test
+    void signup() throws Exception {
+        // given
+        String email = "aaaa@naver.com";
+        String pwd = "1234888888";
+        MemberDTO memberDTO = MemberDTO.of(email, pwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        String jwt = "jwtTokenExample";
+        when(sessionService.createJWTToken(memberDTO)).thenReturn(jwt);
+
+        // when // then
+        mockMvc.perform(
+                        post("/session")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.token").value(jwt));
+
+    }
+
+    @DisplayName("로그인 요청의 이메일 입력값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void login_Email_Exception() throws Exception {
+        // given
+        String wrongEmail = "google.com";
+        String pwd = "1234888888";
+        MemberDTO memberDTO = MemberDTO.of(wrongEmail, pwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        String jwt = "jwtTokenExample";
+        when(sessionService.createJWTToken(memberDTO)).thenReturn(jwt);
+
+        // when // then
+        mockMvc.perform(
+                        post("/session")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(EMAIL_VALID_MSG))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+
+    @DisplayName("로그인 요청의 비밀번호 입력값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void login_Pwd_Exception() throws Exception {
+        // given
+        String email = "ef@google.com";
+        String wrongPwd = "188";
+        MemberDTO memberDTO = MemberDTO.of(email, wrongPwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        String jwt = "jwtTokenExample";
+        when(sessionService.createJWTToken(memberDTO)).thenReturn(jwt);
+
+        // when // then
+        mockMvc.perform(
+                        post("/session")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(PWD_VALID_MSG))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+    @DisplayName("로그인 요청의 이메일과 비밀번호 둘 다 값이 올바르지 않아 예외가 발생한다")
+    @Test
+    void login_Email_Pwd_Exception() throws Exception {
+        // given
+        String wrongEmail = "efgoogle.com";
+        String wrongPwd = "188";
+        MemberDTO memberDTO = MemberDTO.of(wrongEmail, wrongPwd);
+        String requestBody = objectMapper.writeValueAsString(memberDTO);
+        String jwt = "jwtTokenExample";
+        when(sessionService.createJWTToken(memberDTO)).thenReturn(jwt);
+
+        // when // then
+        mockMvc.perform(
+                        post("/session")
+                                .content(requestBody)
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value(Matchers.containsString(EMAIL_VALID_MSG)))
+                .andExpect(jsonPath("$.message").value(Matchers.containsString(PWD_VALID_MSG)))
+                .andExpect(jsonPath("$.code").value("COMMON-001"));
+    }
+}
