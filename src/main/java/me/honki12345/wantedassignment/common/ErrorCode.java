24d09fb8a6a5decc444614559288fc7be9dbe37d
honki12345 <iui4793@gmail.com>
2023-08-11 19:13:15 +0900

    Feat - (ErrorCode) 로그인 인증예외 추가

--- src/main/java/me/honki12345/wantedassignment/common/ErrorCode.java
+++ src/main/java/me/honki12345/wantedassignment/common/ErrorCode.java
@@ -8,6 +8,7 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE("올바르지 않은 입력값입니다.", "COMMON-001", HttpStatus.BAD_REQUEST),
     NOT_FOUND("해당 값은 존재하지 않습니다", "COMMON-002", HttpStatus.NOT_FOUND),
     NOT_AUTHORIZED("권한이 없습니다", "COMMON-003", HttpStatus.FORBIDDEN),
+    NEED_LOGIN("로그인이 필요합니다", "COMMON-004", HttpStatus.UNAUTHORIZED),
 
     MEMBER_NOT_FOUND("해당 유저를 찾을 수 없습니다", "MEMBER-001", HttpStatus.NOT_FOUND),
     EMAIL_DUPLICATE("중복된 이메일이 존재합니다", "MEMBER-002", HttpStatus.BAD_REQUEST),
@@ -23,4 +24,5 @@ public enum ErrorCode {
         this.code = code;
         this.status = status;
     }
+
 }
