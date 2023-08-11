ef80443b46ca671834784832de92289c50d9c94a
honki12345 <iui4793@gmail.com>
2023-08-11 13:37:04 +0900

    Refactor #6 - (SessionController) Service 계층으로 역할분리

--- src/main/java/me/honki12345/wantedassignment/controller/SessionController.java
+++ src/main/java/me/honki12345/wantedassignment/controller/SessionController.java
@@ -1,16 +1,13 @@
 package me.honki12345.wantedassignment.controller;
 
 import lombok.RequiredArgsConstructor;
-import me.honki12345.wantedassignment.config.jwt.TokenProvider;
 import me.honki12345.wantedassignment.dto.MemberDTO;
 import me.honki12345.wantedassignment.dto.TokenDTO;
+import me.honki12345.wantedassignment.service.SessionService;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,26 +19,18 @@ import static me.honki12345.wantedassignment.config.jwt.JwtFilter.*;
 @RequestMapping("/session")
 @RequiredArgsConstructor
 public class SessionController {
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
+    private final SessionService sessionService;
+    
     @PostMapping
-    public ResponseEntity<TokenDTO> login(@RequestBody MemberDTO memberDTO) {
+    public ResponseEntity<TokenDTO> login(@Validated @RequestBody MemberDTO memberDTO) {
         // TODO 로그인 로직 & jwt 토큰값 반환 & 예외처리 & 검증
 
-        UsernamePasswordAuthenticationToken authenticationToken
-                = new UsernamePasswordAuthenticationToken(memberDTO.email(), memberDTO.pwd());
-
-        Authentication authentication
-                = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-
-        String jwt = tokenProvider.createToken(authentication);
+        String jwt = sessionService.createJWTToken(memberDTO);
 
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(AUTHORIZATION_HEADER, AUTHORIZATION_PREFIX + jwt);
 
 
-        return new ResponseEntity<>(new TokenDTO(jwt), httpHeaders, HttpStatus.OK);
+        return new ResponseEntity<>(new TokenDTO(jwt), httpHeaders, HttpStatus.CREATED);
     }
 }
