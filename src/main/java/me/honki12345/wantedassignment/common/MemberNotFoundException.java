2dd6033bc3a1bcf75aab80e3590b863aa0a8f9eb
honki12345 <iui4793@gmail.com>
2023-08-11 09:57:20 +0900

    Refactor - MemberNotFound, PostNotFound 리팩토링

--- src/main/java/me/honki12345/wantedassignment/common/MemberNotFoundException.java
+++ src/main/java/me/honki12345/wantedassignment/common/MemberNotFoundException.java
@@ -1,7 +1,4 @@
 package me.honki12345.wantedassignment.common;
 
 public class MemberNotFoundException extends NotFoundException {
-    public MemberNotFoundException() {
-        super("해당 유저를 찾을 수 없습니다");
-    }
 }
--- src/main/java/me/honki12345/wantedassignment/common/PostNotFoundException.java
+++ src/main/java/me/honki12345/wantedassignment/common/PostNotFoundException.java
@@ -1,7 +1,4 @@
 package me.honki12345.wantedassignment.common;
 
 public class PostNotFoundException extends NotFoundException{
-    public PostNotFoundException() {
-        super("해당 게시글은 존재하지 않습니다");
-    }
 }
